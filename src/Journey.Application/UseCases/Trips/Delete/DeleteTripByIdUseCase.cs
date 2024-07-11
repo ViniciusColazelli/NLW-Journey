ff93ab23ca1c944d57bf727bc4f6317ede20b0a4
vinic <vinic@DESKTOP-D8DMH6E>
2024-07-11 19:07:18 -0300

    Add project files.

---
+++ src/Journey.Application/UseCases/Trips/Delete/DeleteTripByIdUseCase.cs
@@ -0,0 +1,33 @@
+﻿using Journey.Communication.Responses;
+using Journey.Exception.ExceptionsBase;
+using Journey.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey.Application.UseCases.Trips.Delete
+{
+    public class DeleteTripByIdUseCase
+    {
+        public void Execute(Guid id)
+        {
+            var dbContext = new JourneyDbContext();
+
+            var trip = dbContext
+                .Trips
+                .Include(trip => trip.Activities)
+                .FirstOrDefault(trip => trip.Id == id);
+
+            if (trip is null)
+            {
+                throw new NotFoundException(ResourceErrorMessages.TRIP_NOT_FOUND);
+            }
+
+            dbContext .Trips.Remove(trip);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ src/Journey.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace Journey.Exception.ExceptionsBase
+{
+    public class ErrorOnValidationException : JourneyException
+    {
+        private readonly IList<string> _errors;
+
+        public ErrorOnValidationException(IList<string> message) : base(string.Empty)
+        {
+            _errors = message;
+        }
+
+        public override IList<string> GetErrorMessages()
+        {
+            return _errors;
+        }
+
+        public override HttpStatusCode GetstatusCode()
+        {
+            return HttpStatusCode.BadRequest;
+        }
+    }
+}
